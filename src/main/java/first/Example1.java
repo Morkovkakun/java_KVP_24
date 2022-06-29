e4fde6c6c6a058994c5d55b0cdc43a4dc5c9efe2
Vladimir <hahapupkin@gmail,com>
2022-06-29 17:47:52 +0400

    Helloworld

---
+++ src/main/java/first/Example1.java
@@ -0,0 +1,8 @@
+package first;
+
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.print("Hello,Hello,Hello,Hello,Hello,Hello,Hello,Hello,Hello,Hello,");
+    }
+}
