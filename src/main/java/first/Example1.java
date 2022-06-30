5d5e517b378f2bd9b4be3d4fe7b1b1055b675af0
Vladimir <hahapupkin@gmail,com>
2022-06-30 20:43:20 +0400

    lesson5

--- src/main/java/first/Example1.java
+++
@@ -1,8 +0,0 @@
-package first;
-
-
-public class Example1 {
-    public static void main(String[] args) {
-        System.out.print("Hello,Hello,Hello,Hello,Hello,Hello,Hello,Hello,Hello,Hello,");
-    }
-}
